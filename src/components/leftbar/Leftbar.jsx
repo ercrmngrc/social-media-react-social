113d193f9ee2dd8ade8d7d0103c3d44d564a41b1
Eric Garcia <erraga09@gmail.com>
2022-01-09 10:16:28 -0700

    added the components

---
+++ src/components/leftbar/Leftbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./leftbar.css";
+
+export default function Leftbar() {
+    return (
+        <div className = 'leftbar'>
+            leftbar
+        </div>
+    )
+}
