ac817e1ad442d93a04cb39ce3f22c491be68b3b1
Eric Garcia <erraga09@gmail.com>
2022-01-18 15:36:35 -0700

    routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Home from "./pages/home/Home";
-// import Profile from "./pages/profile/Profile";
-// import Login from "./pages/login/Login";
-//import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route exact path = "/" element={<Home />} />
+        <Route exact path = "/login" element={<Login />} />
+        <Route exact path = "/register" element={<Register />} />
+        <Route exact path = "/profile:username" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
